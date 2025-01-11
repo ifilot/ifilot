fc6d42f433c58b131ae73fd48a612c206f936048
ifilot <ivo@ivofilot.nl>
2025-01-11 15:42:28 +0100

    Initial commit

---
+++ scripts/build.py
@@ -0,0 +1,21 @@
+from jinja2 import Environment, FileSystemLoader
+
+# Configure Jinja2 environment
+env = Environment(loader=FileSystemLoader('templates'))
+
+# Load the template
+template = env.get_template('index.tpl')
+
+# Define the data to pass to the template
+data = {
+    "title": "My Jinja2 Page",
+    "message": "This is a minimal example of rendering a webpage with Jinja2.",
+    "items": ["Item 1", "Item 2", "Item 3"]
+}
+
+# Render the template with the data
+output = template.render(data)
+
+# Write the rendered HTML to a file
+with open("README.md", "w") as f:
+    f.write(output)
---
+++ scripts/templates/index.tpl
@@ -0,0 +1,9 @@
+<h1><img src="https://emojis.slackmojis.com/emojis/images/1643517375/34106/coffee_chemistry.gif" width="30"/> Hey there!</h1>
+<p>I am Ivo, computational chemist and retro computer enthousiast :space_invader: from the Netherlands <img src="https://hatscripts.github.io/circle-flags/flags/nl.svg" width="16">.
+<p>{{ message }}</p>
+
+<ul>
+    {% for item in items %}
+    <li>{{ item }}</li>
+    {% endfor %}
+</ul>
