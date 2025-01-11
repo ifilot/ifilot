0ea7a490273def15760f9af7ec1736898184deb0
ifilot <ivo@ivofilot.nl>
2025-01-11 16:28:53 +0100

    Updating

--- scripts/build.py
+++ scripts/build.py
@@ -1,34 +1,74 @@
 from jinja2 import Environment, FileSystemLoader
 import os
+import requests
 
-# Configure Jinja2 environment
-env = Environment(loader=FileSystemLoader(os.path.join(
-    os.path.dirname(__file__), 
-    'templates')
-))
+def main():
+    # Configure Jinja2 environment
+    env = Environment(loader=FileSystemLoader(os.path.join(
+        os.path.dirname(__file__), 
+        'templates')
+    ))
 
-# Load the template
-template = env.get_template('index.tpl')
+    # Load the template
+    template = env.get_template('index.tpl')
 
-language_icons = [
-    'https://img.shields.io/badge/-C-blue?logo=c',
-    'https://img.shields.io/badge/-C++-blue?logo=cplusplus',
-    'https://img.shields.io/badge/python-3670A0?logo=python&logoColor=ffdd54',
-    'https://img.shields.io/badge/PHP-777BB4?logo=php&logoColor=white',
-    'https://img.shields.io/badge/cakephp-red?logo=cakephp&logoColor=black'
-    'https://img.shields.io/badge/asm-6502-red',
-    'https://img.shields.io/badge/asm-z80-red',
-    'https://img.shields.io/badge/asm-8086-red',
-]
+    # show languages / tools
+    language_icons = [
+        'https://img.shields.io/badge/-C-blue?logo=c',
+        'https://img.shields.io/badge/-C++-blue?logo=cplusplus',
+        'https://img.shields.io/badge/python-3670A0?logo=python&logoColor=ffdd54',
+        'https://img.shields.io/badge/PHP-777BB4?logo=php&logoColor=white',
+        'https://img.shields.io/badge/cakephp-red?logo=cakephp&logoColor=white'
+        'https://img.shields.io/badge/asm-6502-orange',
+        'https://img.shields.io/badge/asm-z80-orange',
+        'https://img.shields.io/badge/asm-8086-orange',
+    ]
 
-# Define the data to pass to the template
-data = {
-    'language_icons' : language_icons
-}
+    # fetch repo data
+    repositories = [
+        'hfcxx', 'dftcxx', 'edp', 'den2obj', 'pyqint', 'pydft', 'sphecerix', 'pypwdft'
+    ]
+    ccp_repositories = []
+    for repo in repositories:
+        ccp_repositories.append(fetch_github_repo_details('ifilot', repo))
 
-# Render the template with the data
-output = template.render(data)
+    # Define the data to pass to the template
+    data = {
+        'language_icons' : language_icons,
+        'ccp_repositories' : ccp_repositories
+    }
 
-# Write the rendered HTML to a file
-with open("README.md", "w") as f:
-    f.write(output)
+    # Render the template with the data
+    output = template.render(data)
+
+    # Write the rendered HTML to a file
+    with open("README.md", "w") as f:
+        f.write(output)
+
+def fetch_github_repo_details(owner, repo):
+    url = f"https://api.github.com/repos/{owner}/{repo}"
+    headers = {
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        stars = data.get("stargazers_count", 0)
+        issues = data.get("open_issues_count", 0)
+        link = data.get("html_url", "No link available")
+        forks = data.get("forks_count", 0)
+        description = data.get("description", "No description available")
+        return {
+            "stars": stars,
+            "issues": issues,
+            "forks": forks,
+            "description": description,
+            "link": link,
+        }
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+    
+if __name__ == '__main__':
+    main()
--- scripts/templates/index.tpl
+++ scripts/templates/index.tpl
@@ -1,9 +1,35 @@
 <h1>:wave: Hey there!</h1>
 <p>I am Ivo, computational chemist :test_tube: and retro computer enthousiast :space_invader: from the Netherlands <img src="https://hatscripts.github.io/circle-flags/flags/nl.svg" width="16">.
 
-<h2>Languages I like to program with</h2>
+<h2>I like to program with</h2>
 <p>
 {%- for img in language_icons %}
 <img src="{{ img }}" />
 {%- endfor %}
 </p>
+
+<h2>Computational chemistry projects</h2>
+<table>
+  <thead align="center">
+    <tr border: none;>
+      <td><b>Projects</b></td>
+      <td><b>Stars</b></td>
+      <td><b>Issues</b></td>
+      <td><b>Forks</b></td>
+    </tr>
+  </thead>
+  <tbody>
+  {%- for repo in ccp_repositories %}
+    <tr>
+      <td><a href="{{ repo.link }}">{{ repo.repo }}</a><br>
+          <i>{{ repo.description }}</i>
+      </td>
+      <td>{{ repo.stars }}</td>
+      <td>{{ repo.issues }}</td>
+      <td>{{ repo.forks }}</td>
+    </tr>
+  {%- endfor %}
+  </tbody>
+</table>
+
+<h2>Retro computing</h2>
